d1a4c6b00ff684417a3e6d26b5c31fc66ebda8a5
mesmer <tchoupounathanael@gmail.com>
2023-06-24 23:00:35 +0100

    correct linter errors

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,35 +1,35 @@
-const stringInput = document.querySelector(".input");
-const normalRect = document.querySelector(".normal-text");
-const btnSubmit = document.querySelector(".encode");
-const encoded = document.querySelector(".encoded-area");
-const encodedRect = document.querySelector(".encoded-message");
+const stringInput = document.querySelector('.input')
+const normalRect = document.querySelector('.normal-text')
+const btnSubmit = document.querySelector('.encode')
+const encoded = document.querySelector('.encoded-area')
+const encodedRect = document.querySelector('.encoded-message')
 
-btnSubmit.addEventListener("click", function () {
-  secretMessage();
-});
+btnSubmit.addEventListener('click', function () {
+  secretMessage()
+})
 
-function secretMessage() {
-  const str = stringInput.value;
-  const normalized = str.replace(/[^\w]/g, " ").toLowerCase();
-  const strLength = normalized.length;
-  const size = Math.ceil(Math.sqrt(strLength));
+function secretMessage () {
+  const str = stringInput.value
+  const normalized = str.replace(/[^\w]/g, ' ').toLowerCase()
+  const strLength = normalized.length
+  const size = Math.ceil(Math.sqrt(strLength))
 
-  let encrypt = " ";
-  let encrypText = " ";
+  let encrypt = ' '
+  let encrypText = ' '
 
   if (strLength < 50) {
-    alert("Input Should not be less than 50 characters");
+    alert('Input Should not be less than 50 characters')
   } else {
     for (let i = 0; i < size; i++) {
       for (let j = i; j < strLength; j += size) {
-        encrypt += normalized[j];
-        encrypText += normalized[j];
+        encrypt += normalized[j]
+        encrypText += normalized[j]
       }
-      encrypText += "\n";
+      encrypText += '\n'
     }
 
-    normalRect.innerHTML = `${normalized}`;
-    encoded.innerHTML = `${encrypt}`;
-    encodedRect.innerHTML = `${encrypText}`;
+    normalRect.innerHTML = `${normalized}`
+    encoded.innerHTML = `${encrypt}`
+    encodedRect.innerHTML = `${encrypText}`
   }
 }
